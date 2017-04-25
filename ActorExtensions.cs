b116f5261024b62220ab0e56f619de099b1bb625
Pratap Bhaskar <Pratap.Bhaskar@harman.com>
2017-04-25 09:24:55 +0100

    initial version

---
+++ ActorExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Akka.Actor;
+using IotNext.Akka.Contracts;
+
+namespace Akka.Sample.Extensions
+{
+    public static class ActorExtensions
+    {
+        public static async Task<IActorRef> CreateActorIfNotExistsAsync(this ActorSystem actorSystem,
+            ActorMetadata actorPath, Props creationProps)
+        {
+            IActorRef actor = null;
+            try
+            {
+                actor = await actorSystem.ActorSelection(actorPath.Path)
+                    .ResolveOne(TimeSpan.FromSeconds(1));
+            }
+            catch (ActorNotFoundException)
+            {
+                actor = actorSystem.ActorOf(creationProps, actorPath.Name);
+            }
+            return actor;
+        } 
+    }
+}
---
+++ ActorPaths.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Akka.Sample.Extensions
+{
+    public class ActorMetadata
+    {
+        public string Name { get; private set; }
+        public string Path { get; private set; }
+
+        public ActorMetadata Parent { get; private set; }
+        public ActorMetadata(string name, ActorMetadata parent = null)
+        {
+            Name = name;
+            var parentPath = parent != null ? parent.Path : "/user";
+            Path = $"{parentPath}/{Name}";
+        }
+    }
+}
